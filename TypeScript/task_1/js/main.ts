2cd33ebb650f109547b03e988163df61387cdbea
AnthonyCointre <8732@holbertonstudents.com>
2024-09-24 09:13:09 +0200

    modify main.ts

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -15,6 +15,33 @@ interface Teacher {
     (firstName: string, lastName: string): string;
   }
 
+  interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+  }
+
+  interface StudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+
+  class StudentClass implements StudentClass {
+    private firstName: string;
+    private lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+
   const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
   };
@@ -40,3 +67,7 @@ interface Teacher {
   console.log(director1);
 
   console.log(printTeacher("John", "Doe"));
+
+  const student = new StudentClass('Alice', 'Smith');
+  console.log(student.workOnHomework());
+  console.log(student.displayName());
